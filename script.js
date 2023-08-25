ad2f8c4137da1e47e08be54a180cba0f0cbea6db
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-08-25 18:32:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ const apiUrls = [
 ];
 
 // You can write your code here
+let promises=apiUrls.map((url)=>{
+	return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+      console.error(`Failed to fetch data from ${url}:`, error);
+      throw error;
+    });
+})
+function calculate(p){
+	const start=performance.now()
+	return p.then(()=>performance.now()-start)
+}
+const t1=calculate(Promise.all(promises))
+const t2=calculate(Promise.any(promises))
+console.log(t1)
+Promise.all([t1,t2]).then(([t11,t22])=>{
+	  const outputAll = document.getElementById("output-all");
+  const outputAny = document.getElementById("output-any");
+	console.log("aaaaa",outputAll)
+	// let b=document.getElementById("output-any")
+	outputAll.innerText=t11.toFixed(2)
+	outputAny.innerText=t22.toFixed(2)
+	
+})
